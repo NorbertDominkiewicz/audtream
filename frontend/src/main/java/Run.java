b43de7ae32e72b89354f112975849311326ea884
ndominkiewicz <norbertd1410@gmail.com>
2025-10-24 13:21:11 +0200

    working on frontend module

---
+++ frontend/src/main/java/Run.java
@@ -0,0 +1,7 @@
+public class Run {
+    public static void main(String[] args) throws Exception {
+//        Runtime rt = Runtime.getRuntime();
+//        Process pr = rt.exec("npm run dev");
+        System.out.println("Frontend Application Started");
+    }
+}
