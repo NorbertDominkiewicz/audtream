5b92ec59cb9c0ffe8d99ea2df74add1d99152637
ndominkiewicz <norbertd1410@gmail.com>
2025-10-24 12:01:04 +0200

    lets start the show

---
+++ backend/src/main/java/com/ioproject/audtream/AudtreamApplication.java
@@ -0,0 +1,13 @@
+package com.ioproject.audtream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AudtreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AudtreamApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ioproject/audtream/AudtreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ioproject.audtream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AudtreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
